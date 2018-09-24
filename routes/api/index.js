7f795a161228be99bf8fe690b15505f94c585e9f
Mix Master Mike <mikeyamato@gmail.com>
2018-09-24 12:36:04 -0700

    updated keys path

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const slackEventsApi = require('@slack/events-api');
 const express = require('express');
 const router = express.Router();
 const passport = require('passport'); 
-const keys = require('./config/keys')
+const keys = require('../../config/keys')
 
 
 

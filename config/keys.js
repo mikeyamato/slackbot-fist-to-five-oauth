014b4cd401489ab9d6c5961e8cdd278cc5968e40
Mix Master Mike <mikeyamato@gmail.com>
2018-10-04 10:36:59 -0700

    updated keys file

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-	tokenSlackAccessToken: process.env.TONKOTSU_SLACK_ACCESS_TOKEN,
-	tokenSlackClientId: process.env.TONKOTSU_SLACK_CLIENT_ID,
-	tokenSlackClientSecret: process.env.TONKOTSU_SLACK_CLIENT_SECRET,
-	tokenSlackSigningSecret: process.env.TONKOTSU_SLACK_SIGNING_SECRET,
-	tokenSlackVerificationToken: process.env.TONKOTSU_SLACK_VERIFICATION_TOKEN
-};
+if(process.env.NODE_ENV === 'production'){
+	module.exports = require('./keys_prod');
+} else {
+	module.exports = require('./keys_dev');
+}

3bd019a5e303ed8a63e4cffcdaaac5a749c0ee61
Mix Master Mike <mikeyamato@gmail.com>
2018-10-08 17:10:39 -0700

    updated oauth.js

--- routes/api/oauth.js
+++ routes/api/oauth.js
@@ -207,7 +207,8 @@ function surveyToClass() {
 	
 	// const channelPortion = `?channel=${channelId}`;  
 	const textPortion = surveyQ.text;
-	const attachmentPortion = JSON.stringify(surveyQ.attachments);  // w/o `JSON.stringify`, error of `[object object]`
+  const attachmentPortion = JSON.stringify(surveyQ.attachments);  // w/o `JSON.stringify`, error of `[object object]`
+  attachmentPortionRevised = JSON.parse(attachmentPortion);
 	// const prettyPortion = '&pretty=1';  // no documentation availble about what this does
 
 	const postSurvey = {
@@ -220,7 +221,7 @@ function surveyToClass() {
     body: `{  
       "channel":"${channelId}",
       "text":${textPortion},
-      "attachments": [${attachmentPortion}]
+      "attachments": ${attachmentPortionRevised}
     }`
 	}
 
